563bf9698491bbf3c7cfd015b539c13acbf8e263
John Grubbs <johnmlowery@gmail.com>
2018-07-30 17:43:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   obj[key] = value
   return obj
+<<<<<<< HEAD
 }
 function deleteFromObjectByKey(obj, key) {
   return Object.assign ({}, {obj: key}, {obj: key})
@@ -13,3 +14,6 @@ function deleteFromObjectByKey(obj, key) {
 function destructivelyDeleteFromObjectByKey(obj, key){
   return delete obj[key]
 }
+=======
+}
+>>>>>>> 314af9f97f4496cab53e495287f9cf9be0895083
